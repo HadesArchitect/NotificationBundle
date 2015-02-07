5aefd51a2407241ad0d882e4888de573847093fe
Alexander Volochnev <a.volochnev@gmail.com>
2015-02-07 19:16:32 +0300

    Testing env prepared

---
+++ Tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the notification bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+$loader = @include __DIR__ . '/../vendor/autoload.php';
+if (!$loader) {
+    die(<<<'EOT'
+You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+EOT
+    );
+}
